a0805f881e9ca99de991af3a149d7d740647f885
„klepo313” <antonio.klepo1@gmail.com>
2023-11-07 22:38:30 +0100

    ZDK_polinomi_gotovKod

---
+++ ZDK_polinomi/ZDK_polinomi.c
@@ -0,0 +1,143 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//#define maxPot 5
+//
+////typedef struct Poli* Polinom;
+//
+//typedef struct Polinom {
+//	int Koef;
+//	int Pot;
+//	struct Polinom* next;
+//} Polinom;
+//
+////int loadPol(Polinom *, char pol[]);
+//int sortPol(char pol[], int koef[], int pot[]);
+//int getKoefPot(char[], int[], int[], int);
+////void printPol(Polinom*);
+//
+//int main() {
+//
+//	//Inicijalizacija i provjera file-a
+//	FILE* file;
+//	file = fopen("polinomi.txt", "r");
+//	if (file == NULL) {
+//		printf("Datoteka nije pronadjena\n");
+//		return -1;
+//	}
+//
+//	char px[50] = {}, qx[50] = {};
+//	int koef_px[10] = { 0 }, koef_qx[10] = { 0 },
+//		pot_px[10] = { 0 }, pot_qx[10] = { 0 },
+//		noChar_px = 0, noChar_qx = 0;
+//
+//	if (fgets(px, sizeof(px), file) != NULL) {
+//		printf("P(x) = %s", px);
+//	}
+//	if (fgets(qx, sizeof(qx), file) != NULL) {
+//		printf("Q(x) = %s\n", qx);
+//	}
+//	else {
+//		printf("Greška pri citanju iz datoteke.\n");
+//		fclose(file);
+//		return -1;
+//	}
+//
+//	printf("\nP(x) atributi:\n");
+//	getKoefPot(px, koef_px, pot_px, noChar_px);
+//
+//	printf("\nQ(x) atributi:\n");
+//	getKoefPot(qx, koef_qx, pot_qx, noChar_qx);
+//
+//	//Liste za dva polinoma Px i Qx
+//	/*Polinom* Px = NULL, * Qx = NULL;
+//	Px = (Polinom* )malloc(sizeof(Polinom));
+//	Qx = Px;
+//
+//	if (Px == NULL || Qx == NULL) {
+//		printf("Greska pri din. alokaciji liste\n");
+//		return -1;
+//	}*/
+//
+//	//Sortiranje polinoma
+//	//sortPol(px, )
+//
+//	/*if (loadPol(Px, px) == 1) {
+//		printPol(Px);
+//	}
+//	if (loadPol(Qx, qx) == 1) {
+//		printPol(Px);
+//	} 
+//	else {
+//		printf("Krivo ucitana lista\n");
+//		return -1;
+//	}*/
+//
+//
+//	return 0;
+//}
+//
+////void printPol(Polinom *head) {
+////	Polinom* temp = head;
+////	temp = temp->next;
+////
+////	if (temp == NULL)
+////		printf("Lista je prazna!\n");
+////
+////	while (temp != NULL){
+////		printf("%dx^%d ", temp->Koef, temp->Pot);
+////		temp = temp->next;
+////	}
+////}
+//
+////int loadPol(Polinom *head, char pol[]) {
+////	Polinom* newPol = NULL;
+////	newPol = (Polinom*)malloc(sizeof(Polinom));
+////	
+////	char ch[255] = {};
+////
+////	for (int i = 0; i < sizeof(pol); i++) {
+////		ch[i] = pol[i];
+////
+////		if (ch[i] == 'x') {
+////			return 1;
+////		}
+////	}
+////}
+//
+//int getKoefPot(char pol[], int koef[], int pot[], int noChar) {
+//	char* token = strtok(pol, "+"); // Razdvajamo string na članove koristeći znak '+'
+//
+//	while (token != NULL) {
+//		char* x_pos = strchr(token, 'x'); // Pronalazimo poziciju znaka 'x' u trenutnom članu
+//
+//		if (x_pos != NULL) {
+//			// Ako postoji 'x' u trenutnom članu
+//			char* caret_pos = strchr(x_pos, '^'); // Pronalazimo poziciju znaka '^'
+//			if (caret_pos != NULL) {
+//				*caret_pos = '\0'; // Postavljamo null terminator nakon 'x'
+//				pol[noChar] = atoi(token); // Pretvaramo koeficijent u cijeli broj
+//				pot[noChar] = atoi(caret_pos + 1); // Preskačemo '^' i pretvaramo potenciju u cijeli broj
+//			}
+//		}
+//		else {
+//			// Ako nema 'x' u trenutnom članu
+//			pol[noChar] = atoi(token); // Koeficijent je cijeli broj iz trenutnog člana
+//			pot[noChar] = 0; // Potencija je 0
+//		}
+//		noChar++;
+//		token = strtok(NULL, "+"); // Prelazimo na sljedeći član
+//	}
+//
+//	// Ispis koeficijenata i potencija
+//	for (int i = 0; i < noChar; i++) {
+//		printf("Koeficijent %d: %d, Potencija %d: %d\n", i + 1, koef[i], i + 1, pot[i]);
+//	}
+//	return 1;
+//}
+//
+//int sortPol(char pol[], int koef[], int pot[]) {
+//	return 1;
+//}
---
+++ ZDK_polinomi/ZDK_polinomi_RIJESEN.c
@@ -0,0 +1,122 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Polinom {
+    int Koef;
+    int Pot;
+    struct Polinom* next;
+} Polinom;
+
+Polinom* dodajNaPocetak(Polinom* head, int koef, int pot);
+void ispisPolinoma(Polinom* head);
+void oslobodiListu(Polinom* head);
+
+int main() {
+    FILE* datoteka = fopen("polinomi.txt", "r");
+    if (datoteka == NULL) {
+        printf("Greška pri otvaranju datoteke.\n");
+        return 1;
+    }
+
+    int koef[2][50] = { {0} };  // Inicijalizacija nizova za koeficijente za dva polinoma
+    int pot[2][50] = { {0} };   // Inicijalizacija nizova za potencije za dva polinoma
+    int broj_clanova[2] = { 0 }; // Brojači članova za dva polinoma
+
+    char linija[255];
+
+    int trenutni_polinom = 0; // Varijabla koja prati trenutni polinom (0 za prvi, 1 za drugi)
+
+    while (fgets(linija, sizeof(linija), datoteka) != NULL) {
+        if (strcmp(linija, "P(x)\n") == 0) {
+            trenutni_polinom = 0;
+            continue;
+        }
+        else if (strcmp(linija, "Q(x)\n") == 0) {
+            trenutni_polinom = 1;
+            continue;
+        }
+
+        if (sscanf(linija, "%dx^%d", &koef[trenutni_polinom][broj_clanova[trenutni_polinom]], &pot[trenutni_polinom][broj_clanova[trenutni_polinom]]) == 2) {
+            broj_clanova[trenutni_polinom]++;
+        }
+    }
+
+    fclose(datoteka);
+
+    // Sortiranje članova po potencijama za oba polinoma
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < broj_clanova[i] - 1; j++) {
+            for (int k = j + 1; k < broj_clanova[i]; k++) {
+                if (pot[i][j] < pot[i][k]) {
+                    // Zamijeni koeficijente i potencije ako je potencija veća
+                    int temp_koef = koef[i][j];
+                    int temp_pot = pot[i][j];
+
+                    koef[i][j] = koef[i][k];
+                    pot[i][j] = pot[i][k];
+
+                    koef[i][k] = temp_koef;
+                    pot[i][k] = temp_pot;
+                }
+            }
+        }
+    }
+
+    // Ispis sortiranih članova
+    for (int i = 0; i < 2; i++) {
+        printf("Sortirani polinom %c(x) = ", i == 0 ? 'P' : 'Q');
+        for (int j = 0; j < broj_clanova[i]; j++) {
+            printf("%+dx^%d ", koef[i][j], pot[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Stvaranje i ispis liste za oba polinoma
+    Polinom* polinom_head[2] = { NULL };
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < broj_clanova[i]; j++) {
+            polinom_head[i] = dodajNaPocetak(polinom_head[i], koef[i][j], pot[i][j]);
+        }
+        printf("Polinom %c(x) kao lista:\n", i == 0 ? 'P' : 'Q');
+        ispisPolinoma(polinom_head[i]);
+    }
+
+    // Oslobađanje memorije
+    for (int i = 0; i < 2; i++) {
+        oslobodiListu(polinom_head[i]);
+    }
+
+    return 0;
+}
+
+Polinom* dodajNaPocetak(Polinom* head, int koef, int pot) {
+    Polinom* novi = (Polinom*)malloc(sizeof(Polinom));
+    if (novi == NULL) {
+        printf("Greška pri alokaciji memorije.\n");
+        exit(1);
+    }
+    novi->Koef = koef;
+    novi->Pot = pot;
+    novi->next = head;
+    return novi;
+}
+
+void ispisPolinoma(Polinom* head) {
+    Polinom* trenutni = head;
+    while (trenutni != NULL) {
+        printf("%+dx^%d ", trenutni->Koef, trenutni->Pot);
+        trenutni = trenutni->next;
+    }
+    printf("\n");
+}
+
+void oslobodiListu(Polinom* head) {
+    Polinom* trenutni = head;
+    while (trenutni != NULL) {
+        Polinom* temp = trenutni;
+        trenutni = trenutni->next;
+        free(temp);
+    }
+}
