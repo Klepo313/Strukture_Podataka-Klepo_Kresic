03c84c77f0bf2c53f45bf3e7ddac0ba5f576f7db
„klepo313” <antonio.klepo1@gmail.com>
2023-12-13 17:06:45 +0100

    Riješen 7. zdk

---
+++ ZDK_7/ZDK_7.c
@@ -0,0 +1,26 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//typedef struct Directory {
+//	int nameOfRoot[10];
+//	struct Directory* child;
+//	struct Directory* nextChild;
+//} Directory;
+//
+//int main() {
+//
+//	Directory* root = NULL;
+//	root = (Directory*)malloc(sizeof(Directory));
+//	printf("Unesi ime root direktorija: ");
+//	scanf(" %s", root->nameOfRoot);
+//
+//	//printf("%s:/", root->nameOfRoot);
+//
+//	while (1) {
+//		
+//	}
+//
+//	return 0;
+//}
